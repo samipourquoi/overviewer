a9e07b83af37aa5e14e406e1d51c257b9ceca658
samipourquoi <sami.guiderdoni@gmail.com>
2020-11-24 00:58:27 +0100

    Add minecraft -> screen coordinates transformation

--- src/render.c
+++ src/render.c
@@ -5,8 +5,7 @@
 #include "../parson/parson.h"
 
 char* get_block_path(char* name);
-void draw_model_cube_all(cairo_t* cr, JSON_Object* textures);
-void draw_block(cairo_t* cr, char* name);
+void draw_block(cairo_t* cr, char* name, int x, int y);
 cairo_surface_t* render_block(char* name, direction_t direction);
 void draw_texture(cairo_t* cr, char* name, int x, int y, unsigned char direction);
 
@@ -22,50 +21,64 @@ char* get_block_path(char* name) {
 	return path;
 }
 
-void draw_model_cube_all(cairo_t* cr, JSON_Object* textures) {
+#define DRAW_ARGS cairo_t* cr, JSON_Object* textures, int x, int y
+
+void draw_model_cube_all(DRAW_ARGS) {
 	char texture_name[50];
 	const char* texture = json_object_get_string(textures, "all");
+
 	memcpy(texture_name, &texture[16], 50);
-	draw_texture(cr, texture_name, 0, 0, LEFT | RIGHT | TOP);
+	draw_texture(cr, texture_name, x, y, LEFT | RIGHT | TOP);
 }
 
-void draw_model_cube(cairo_t* cr, JSON_Object* textures) {
+void draw_model_cube(DRAW_ARGS) {
 	char texture_name[50];
 	char* top = json_object_get_string(textures, "up");
 	const char* south = json_object_get_string(textures, "south");
 	const char* east = json_object_get_string(textures, "east");
 
 	memcpy(texture_name, &top[16], 50);
-	draw_texture(cr, texture_name, 0, 0, TOP);
+	draw_texture(cr, texture_name, x, y, TOP);
 	memcpy(texture_name, &south[16], 50);
-	draw_texture(cr, texture_name, 0, 0, LEFT);
+	draw_texture(cr, texture_name, x, y, LEFT);
 	memcpy(texture_name, &east[16], 50);
-	draw_texture(cr, texture_name, 0, 0, RIGHT);
+	draw_texture(cr, texture_name, x, y, RIGHT);
 }
 
-void draw_model_cube_bottom_top(cairo_t* cr, JSON_Object* textures) {
+void draw_model_cube_bottom_top(DRAW_ARGS) {
 	char texture_name[50];
 	char* top = json_object_get_string(textures, "top");
 	const char* side = json_object_get_string(textures, "side");
 
 	memcpy(texture_name, &top[16], 50);
-	draw_texture(cr, texture_name, 0, 0, TOP);
+	draw_texture(cr, texture_name, x, y, TOP);
 	memcpy(texture_name, &side[16], 50);
-	draw_texture(cr, texture_name, 0, 0, LEFT | RIGHT);
+	draw_texture(cr, texture_name, x, y, LEFT | RIGHT);
 }
 
-void draw_model_cube_column(cairo_t* cr, JSON_Object* textures) {
+void draw_model_cube_column(DRAW_ARGS) {
 	char texture_name[50];
 	char* end = json_object_get_string(textures, "end");
 	const char* side = json_object_get_string(textures, "side");
 
 	memcpy(texture_name, &end[16], 50);
-	draw_texture(cr, texture_name, 0, 0, TOP);
+	draw_texture(cr, texture_name, x, y, TOP);
 	memcpy(texture_name, &side[16], 50);
-	draw_texture(cr, texture_name, 0, 0, LEFT | RIGHT);
+	draw_texture(cr, texture_name, x, y, LEFT | RIGHT);
 }
 
-void draw_block(cairo_t* cr, char* name) {
+void pos_to_iso(int* x, int* y) {
+	*x *= 14;
+	*y *= 14;
+	double new_x = *x + *y;
+	double new_y = -.5 * (double)(*x) + .5 * (double)(*y);
+	*x = (int)(new_x);
+	*y = (int)(new_y);
+}
+
+void draw_block(cairo_t* cr, char* name, int x, int y) {
+	pos_to_iso(&x, &y);
+
 	char model_name[50];
 	{
 		// "assets/blockstates/": 19
@@ -110,14 +123,14 @@ void draw_block(cairo_t* cr, char* name) {
 		JSON_Object* textures = json_object_get_object(root, "textures");
 		char* parent = json_object_get_string(root, "parent");
 		if (strcmp(parent, "minecraft:block/cube_all") == 0) {
-			draw_model_cube_all(cr, textures);
+			draw_model_cube_all(cr, textures, x, y);
 		} else if (strcmp(parent, "minecraft:block/cube") == 0) {
-			draw_model_cube(cr, textures);
+			draw_model_cube(cr, textures, x, y);
 		} else if (strcmp(parent, "minecraft:block/cube_column") == 0
 					|| strcmp(parent, "minecraft:block/cube_column_horizontal") == 0) {
-			draw_model_cube_column(cr, textures);
+			draw_model_cube_column(cr, textures, x, y);
 		} else if (strcmp(parent, "minecraft:block/cube_bottom_top") == 0) {
-			draw_model_cube_bottom_top(cr, textures);
+			draw_model_cube_bottom_top(cr, textures, x, y);
 		}
 
 		free(model_path);
@@ -131,6 +144,8 @@ cairo_surface_t* render_block(char* name, direction_t direction) {
 	cairo_surface_t* iso = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 32, 32);;
 	cairo_t* iso_cr = cairo_create(iso);
 
+	#define COS_30 0.86602540378
+
 	// Transformation matrices that apply a scaling
 	// and a shearing. It also applies a rotation for the TOP side.
 	// Go look at: http://jeroenhoek.nl/articles/svg-and-isometric-projection.html
@@ -139,7 +154,7 @@ cairo_surface_t* render_block(char* name, direction_t direction) {
 	default:
 	case TOP:
 		matrix.x0 = 15;     matrix.y0 = 0;
-		matrix.xx = .866;   matrix.xy = -.933;
+		matrix.xx = COS_30;   matrix.xy = -.933;
 		matrix.yx = .5;     matrix.yy = .461;
 		break;
 	case LEFT:
@@ -189,8 +204,20 @@ int render() {
 	cairo_t *cr;
 	surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, 100, 100);
 	cr = cairo_create(surface);
+	cairo_set_antialias(cr, CAIRO_ANTIALIAS_NONE);
 
-	draw_block(cr, "ancient_debris");
+
+	draw_block(cr, "crafting_table", 0, 0);
+	// draw_block(cr, "red_wool", -1, 1);
+	// draw_block(cr, "stone", 1, 2);
+	// draw_block(cr, "stone", 0, 2);
+	// draw_block(cr, "gold_ore", 1, 3);
+	// draw_block(cr, "stone", 0, 3);
+
+	// draw_block(cr, "lime_wool", 0, 1);
+	// draw_block(cr, "netherrack", 28, 0);
+	// draw_block(cr, "crafting_table", 13, 23);
+	// draw_block(cr, "target", 24, 24);
 
 	cairo_surface_write_to_png(surface, "render.png");
 
