4c414410a6c905b9b47168bdb55ea862882a35e1
samipourquoi <sami.guiderdoni@gmail.com>
2021-03-21 12:20:17 +0100

    Unfold model textures aliases

--- pilchard/src/blocks/model.rs
+++ pilchard/src/blocks/model.rs
@@ -6,6 +6,7 @@ use std::path::{Path, PathBuf};
 use std::fs;
 use serde::Deserialize;
 use std::borrow::Borrow;
+use std::ops::Deref;
 
 lazy_static! {
     static ref CACHE: Mutex<HashMap<&'static str, &'static str>> = {
@@ -20,26 +21,26 @@ pub fn init() {
         .map( |file| file.unwrap().path() )
         .map(Model::parse)
         .collect();
-    // let model = Model::parse(PathBuf::from("./assets/models/block/cube_all.json"));
+    // let model = Model::parse(PathBuf::from("./assets/models/block/bricks.json"));
 }
 
 #[derive(Deserialize, Debug)]
-struct Model {
+pub struct Model {
     parent: Option<String>,
-    textures: Option<HashMap<String, PathBuf>>,
-    elements: Option<Vec<Element>>
+    pub textures: Option<HashMap<String, String>>,
+    pub elements: Option<Vec<Element>>
 }
 
 #[derive(Deserialize, Debug)]
-struct Element {
+pub struct Element {
     from: [f32; 3],
     to: [f32; 3],
     rotation: Option<Rotation>,
-    faces: Faces
+    pub faces: Faces
 }
 
 #[derive(Deserialize, Debug)]
-struct Rotation {
+pub struct Rotation {
     origin: [f32; 3],
     axis: String,
     angle:  f32,
@@ -47,27 +48,27 @@ struct Rotation {
 }
 
 #[derive(Deserialize, Debug)]
-struct Faces {
-    up:    Option<Face>,
-    down:  Option<Face>,
-    north: Option<Face>,
-    south: Option<Face>,
-    east:  Option<Face>,
-    west:  Option<Face>
+pub struct Faces {
+    pub up:    Option<Face>,
+    pub down:  Option<Face>,
+    pub north: Option<Face>,
+    pub south: Option<Face>,
+    pub east:  Option<Face>,
+    pub west:  Option<Face>
 }
 
 #[derive(Deserialize, Debug)]
-struct Face {
-    uv: Option<[f32; 4]>,
-    texture: String,
-    cullface: Option<Direction>,
-    rotation: Option<i32>,
-    tintindex: Option<i32>
+pub struct Face {
+    pub uv: Option<[f32; 4]>,
+    pub texture: String,
+    pub cullface: Option<Direction>,
+    pub rotation: Option<i32>,
+    pub tintindex: Option<i32>
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
-enum Direction {
+pub enum Direction {
     Up,     Down,
     North,  South,
     East,   West,
@@ -91,7 +92,8 @@ impl Model {
     }
 
     pub fn parse(path: PathBuf) -> Model {
-        let contents = fs::read_to_string(path)
+
+        let contents = fs::read_to_string(&path)
             .unwrap();
         let mut model: Model = serde_json::from_str(contents.as_str())
             .unwrap();
@@ -100,8 +102,41 @@ impl Model {
             let parent = Model::parse(path);
             model.inherit(parent);
         }
+        model.unfold_textures();
 
         model
+    }
+
+    // Each field in 'textures' can either be an identifier to a texture,
+    // or a pointer to another key in 'textures'. If the latter, it will
+    // start with an #.
+    // This effectively makes it a linked list, so we 'unfold' it with this
+    // function to make it easier to bind textures later on.
+    fn unfold_textures(&mut self) {
+        if self.textures.is_none() { return; }
+
+        let textures = self.textures.clone()
+            .unwrap();
+
+        fn dereference_pointer(pointer: &String, textures: &HashMap<String, String>) -> String {
+            if pointer.starts_with('#') {
+                let shortened_ptr = pointer[1..].to_string()
+                    .clone();
+                let value = textures.get(&shortened_ptr);
+                if value.is_none() {
+                    return pointer.clone();
+                }
+
+                dereference_pointer(value.unwrap(), textures).clone()
+            } else {
+                pointer.clone()
+            }
+        }
+
+        let textures: HashMap<_, _> = textures.iter()
+            .map( |(k,v)| (k.clone(), dereference_pointer(v, &textures)))
+            .collect();
+        self.textures = Some(textures);
     }
 
     fn inherit(&mut self, parent: Model) {
@@ -122,6 +157,7 @@ impl Model {
                 .unwrap()
                 .clone();
             b.extend(a);
+            self.textures = Some(b);
         }
     }
 }
